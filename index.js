4927393c41026c0d92f0d1f0d20152dbf09ba19d
helio-frota <00hf11@gmail.com>
2016-09-11 20:28:39 -0300

    Hail!

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ const searcher = require('./lib/searcher');
 const reporter = require('./lib/reporter');
 
 function run (directory) {
-  reader.read('./package.json')
-    .then(x => {
-      const dependencies = searcher.searchDependencies(x);
-      reader.find(directory).forEach(file => {
-        reader.read(file)
-          .then(lines => {
-            const declarations = searcher.searchDeclarations(lines, dependencies);
-            const result = searcher.searchUsage(lines, file, declarations);
-            if (result.length) {
-              reporter.report(result);
-            }
-          });
-      });
-    });
+  const packageJson = reader.read(directory + '/package.json');
+  const dependencies = searcher.searchDependencies(packageJson);
+  const files = reader.find(directory);
+  const result = [];
+  files.forEach(file => {
+    const lines = reader.read(file);
+    const declarations = searcher.searchDeclarations(lines, dependencies);
+    const usage = searcher.searchUsage(lines, file, declarations);
+    if (usage.length) {
+      result.push(usage);
+    }
+  });
+  reporter.report(result);
 }
 
-run('.');
+run('../litoria');
