9a93be0c6bb1cb3f0885651e230dd61d1517e9b4
helio-frota <00hf11@gmail.com>
2016-09-10 23:37:27 -0300

    cleanup

--- eslint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-npm install -D eslint
-npm install -D eslint-plugin-promise eslint-plugin-standard eslint-plugin-react 
-npm install -D eslint-config-standard
-npm install -D eslint-config-semistandard
