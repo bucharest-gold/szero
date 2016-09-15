83759a020dc8c8079c95fa1e6bab6845fa6eeb6d
helio-frota <00hf11@gmail.com>
2016-09-15 09:17:48 -0300

    More testes added.
    Addesses #1

--- test/szero-test.js
+++ test/szero-test.js
@@ -2,6 +2,8 @@
 
 const test = require('tape');
 const log = require('../lib/log-color');
+const reader = require('../lib/reader');
+const path = require('path');
 const stdout = require('test-console').stdout;
 
 test('Should test log-color.', t => {
@@ -15,5 +17,17 @@ test('Should test log-color.', t => {
   t.equal('\x1b[31m[ 1 ]\x1b[39m', log.applyColor(1));
   t.equal('\x1b[33m[ 2 ]\x1b[39m', log.applyColor(2));
   t.equal('\x1b[32m[ 99 ]\x1b[39m', log.applyColor(99));
+  t.end();
+});
+
+test('Should read a file.', t => {
+  const lines = reader.read(path.join(__dirname, '/fixtures/foo/x.js'));
+  t.equal(lines.toString().includes('require'), true);
+  t.end();
+});
+
+test('Should find javascript files.', t => {
+  const files = reader.find(path.join(__dirname, '../.'));
+  t.equal(files.toString().includes('reader.js'), true);
   t.end();
 });
