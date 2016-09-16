f5f1c2b5f028cf706c7f7b46059a7cb890f1b299
Lucas Holmquist <lholmqui@redhat.com>
2016-09-16 14:15:29 -0400

    update test for cli module

--- test/szero-cli-test.js
+++ test/szero-cli-test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const test = require('tape');
+const path = require('path');
 
 const cli = require('../bin/cli');
 
@@ -12,6 +13,13 @@ test('Should export a function', (t) => {
 test('Should fail with no package.json', (t) => {
   cli('package_with_no_package_json').then(null, (err) => {
     t.equal('package.json is require', err, 'should have the package.json is required message');
+    t.end();
+  });
+});
+
+test('Should pass and return an object with real project', (t) => {
+  cli(path.join(__dirname, '/fixtures')).then((report) => {
+    t.pass();
     t.end();
   });
 });
