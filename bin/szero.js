bacd283709c8739dd63d9b351d62d285281c800c
Lucas Holmquist <lholmqui@redhat.com>
2016-09-22 13:46:48 -0400

    refactor to use commander syntax.  --reporter is now --file

--- bin/szero.js
+++ bin/szero.js
@@ -1,55 +1,34 @@
 #! /usr/bin/env node
 
 const run = require('./cli');
-const log = require('../lib/log-color');
+const program = require('commander');
 
-const processArgs = process.argv;
+program
+  .version(require('../package.json').version)
+  .usage('directory [--options]')
+  .option('-f --file', 'enable file reporter')
+  .option('--ci', 'enables process.exit() when unused dependency found')
+  .option('--dev', 'enables devDependencies processing.')
+  .option('--summary', 'enables summary report')
+  .parse(process.argv);
 
-if (processArgs.length >= 3) {
-  // Default the console option when calling from the command line
-  const options = {
-    consoleReporter: true
-  };
 
-  // Find the --reporter= argv
-  // Split on = and read the options
-  const reporters = processArgs.filter((p) => {
-    return p.indexOf('--reporters=') !== -1;
-  });
-
-  if (reporters.length !== 0) {
-    const splitReporters = reporters[0].split('=');
-    if (splitReporters[1] === 'file') {
-      options.fileReporter = true;
-    }
-  }
-
-  // If --ci=true, enables process.exit() when unused dependency found.
-  const ci = processArgs.filter(p => p.includes('--ci='));
-  if (ci.length) {
-    if (ci[0].split('=')[1] === 'true') {
-      options.ci = true;
-    }
-  }
-
-  // If --dev=true, enables devDependencies processing.
-  const dev = processArgs.filter(p => p.includes('--dev='));
-  if (dev.length) {
-    if (dev[0].split('=')[1] === 'true') {
-      options.dev = true;
-    }
-  }
-
-  // If --summary=true, Show only summary report.
-  const summary = processArgs.filter(p => p.includes('--summary='));
-  if (summary.length) {
-    if (summary[0].split('=')[1] === 'true') {
-      options.summary = true;
-      options.dev = true;
-    }
-  }
-
-  run(processArgs[2], options);
-} else {
-  log.red('Usage: szero directory_here');
+if (program.args.length === 0) {
+  program.help();
 }
+
+// Default the console option when calling from the command line
+const options = {
+  consoleReporter: true
+};
+
+options.fileReporter = program.file;
+options.ci = program.ci;
+options.dev = program.dev;
+
+if (program.summary) {
+  options.dev = true;
+  options.summary = true;
+}
+
+run(program.args[0], options);
