611894aad7ccf5d7acbc23492d21372e93ab116d
Lucas Holmquist <lholmqui@redhat.com>
2016-10-05 14:14:40 -0400

    adding in API docs and a slight restructure

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+/**
+* @module szero
+*/
+
 'use strict';
 
 const cli = require('./bin/cli');
 const reporter = require('./lib/reporter');
 
+/**
+  JSON Reporter
+  @param {string} directory - path to your project
+  @param {object} [options]
+  @returns {Promise} - returns a Promise with the repoter values as JSON
+  @instance
+*/
+function report (directory, options) {
+  return cli(directory, options);
+}
+
+/**
+  File Format Reporter
+  @param {string} directory - path to your project
+  @param {object} [options]
+  @returns {Promise} - returns a Promise with the repoter values as a String in the File Format
+  @instance
+*/
+function fileReport (directory, options) {
+  return this.report(directory, options).then((jsonReport) => {
+    const createFileReport = reporter.createFileReport(jsonReport);
+    return Promise.resolve(createFileReport);
+  });
+}
+
 module.exports = {
-  report: function (directory, options) {
-    return cli(directory, options);
-  },
-  fileReport: function (directory, options) {
-    return this.report(directory, options).then((jsonReport) => {
-      const createFileReport = reporter.createFileReport(jsonReport);
-      return Promise.resolve(createFileReport);
-    });
-  }
+  report: report,
+  fileReport: fileReport
 };
